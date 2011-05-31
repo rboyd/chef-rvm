36212a05a2c661b8bc890bf0c851e4b8d38618ee
Robert Boyd <rboyd@telematter.com>
2011-05-31 00:32:58 -0500

    Added workaround for rvm's shell_wrapper.sh which fails to generate valid YAML for multi-line environment variables. See https://github.com/fnichol/chef-rvm/issues/21

---
+++ recipes/shell_wrapper_workaround.rb
@@ -0,0 +1,6 @@
+template "/usr/lib/ruby/gems/1.8/gems/rvm-1.6.16/lib/rvm/shell/shell_wrapper.sh" do
+  source    "shell_wrapper.sh"
+  owner     "root"
+  group     "root"
+  mode      "0664"
+end
---
+++ templates/default/shell_wrapper.sh
@@ -0,0 +1,14 @@
+# Prints an epilog to a shell command.
+__rvm_show_command_epilog() {
+  local last_command_result="$?"
+  export SUDO_COMMAND=""
+  echo "---------------RVM-RESULTS-START---------------"
+  echo "---"
+  echo " exit_status: \"$last_command_result\""
+  echo " environment:"
+  \env | \sed \
+    -e "s#'#\\'#g" \
+    -e 's#"#\\"#g' \
+    -e "s#\\([^=]*\\)=\\(.*\\)#   '\1': \"\2\"#"
+  echo "----------------RVM-RESULTS-END----------------"
+}
